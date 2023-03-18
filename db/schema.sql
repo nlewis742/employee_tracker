8ca4473f664db2adb57504ccbd1b28eae9f7d143
Natasha <nlackas12@gmail.com>
2023-03-18 15:30:17 -0500

    added information to seeds

--- db/schema.sql
+++ db/schema.sql
@@ -28,25 +28,26 @@ CREATE TABLE department (
     dept_name VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE role (
+CREATE TABLE roles (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INT NOT NULL,
     FOREIGN KEY (department_id)
     REFERENCES department(id)
-    ON DELETE CASCADE
-)
+    -- ON DELETE CASCADE
+);
 
 CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
-    manager_id INT NOT NULL
+    manager_id INT,
     FOREIGN KEY (role_id)
-    REFERENCES role(id),
+    REFERENCES roles(id)
+    -- ON DELETE CASCADE,
     FOREIGN KEY (manager_id)
     REFERENCES employee(id)
-    ON DELETE CASCADE
-)
+    -- ON DELETE CASCADE
+);
