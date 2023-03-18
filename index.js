e5893880c99a22d2a4ee1ea06d99e1ed0df8efdc
Natasha <nlackas12@gmail.com>
2023-03-18 16:15:44 -0500

    readme title and function structure

--- index.js
+++ index.js
@@ -12,8 +12,51 @@ inquirer
     message: "What would you like to do?",
     choices: ["View All Employees", "Add Employee", "Update Employee Role", "View All Roles", "Add Role", "View All Departments", "Add Department"],
   },
-
 ])
-.then((answers) => {
-
+.then((selected) => {
+    const { main } = selected;
+    if (main == "View All Employees") {
+        viewEmployees(); 
+    } else if (main == "Add Employee") {
+        addEmployee();
+    } else if (main == "Update Employee Role") {
+        updateRole();
+    }else if (main == "View All Roles") {
+        viewRoles();
+    } else if (main == "Add Role") {
+        addRole();
+    } else if (main == "View All Departments") {
+        viewDept();
+    } else if (main == "Add Department")
+        addDept();
 });
+
+
+
+function viewEmployees() {
+
+};
+
+function addEmployee() {
+
+};
+
+function updateRole() {
+
+};
+
+function viewRoles() {
+
+};
+
+function addRole() {
+
+};
+
+function viewDept() {
+
+};
+
+function addDept() {
+
+};
